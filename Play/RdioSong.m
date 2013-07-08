11e19a1d7afa225c7ecb655c0d3799a301447f0f
Nathan Borror <nathan@nathanborror.com>
2013-07-07 18:55:32 -0700

    Hack to show larger artwork from Rdio

--- Play/RdioSong.m
+++ Play/RdioSong.m
@@ -11,4 +11,17 @@
 @implementation RdioSong
 @synthesize key, name, album, albumArt;
 
+- (NSString *)albumArt
+{
+  NSString *url = self->albumArt;
+
+  NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"square-200" options:0 error:nil];
+  NSTextCheckingResult *match = [regex firstMatchInString:url options:0 range:NSMakeRange(0, url.length)];
+
+  if (match) {
+    return [url stringByReplacingOccurrencesOfString:@"square-200" withString:@"square-600"];
+  }
+  return url;
+}
+
 @end
